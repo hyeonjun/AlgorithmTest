6a792d53fa1e8c57e1eac52c391a7bb82f274c8c
zlel175 <zlel175@naver.com>
2021-05-15 16:33:27 +0900

    2021_05_15

--- DataStructure/Quick_Sort.py
+++ DataStructure/Quick_Sort.py
@@ -10,7 +10,7 @@ def quick_sort(data): # O(n log n) 최악 : O(N^2)
     # for i in range(1, len(data)):
     #     left.append(data[i]) if pivot > data[i] else right.append(data[i])
 
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    return quick_sort(left) + [pivot] + quick_sort(right) # 재귀용법
 import random
 data = random.sample(range(100), 10)
 print(data)
