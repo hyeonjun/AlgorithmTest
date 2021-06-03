abdf4f5c8db259fed09d1a6c3ee0bfacaea87cf8
zlel175 <zlel175@naver.com>
2021-06-03 20:36:51 +0900

    Commit

--- ProGrammers/Lv1/level_1_Dart_Game.py
+++ ProGrammers/Lv1/level_1_Dart_Game.py
@@ -26,12 +26,12 @@ def solution(dartResult):
             answer[i] = value
     return sum(answer)
 
-# print(solution("1S2D*3T"))
-# print(solution("1D2S#10S"))
-# print(solution("1D2S0T"))
-# print(solution("1S*2T*3S"))
-# print(solution("1D#2S*3S"))
-# print(solution("1T2D3D#"))
+print(solution("1S2D*3T"))
+print(solution("1D2S#10S"))
+print(solution("1D2S0T"))
+print(solution("1S*2T*3S"))
+print(solution("1D#2S*3S"))
+print(solution("1T2D3D#"))
 print(solution("1D2S3T*"))
 
 def solution(dartResult):
