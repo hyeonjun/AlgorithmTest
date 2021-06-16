ea328cc22e5fdb38f4e4e5a2ae51336cd122fe3d
zlel175 <zlel175@naver.com>
2021-06-16 16:20:21 +0900

    2021_06_16_Programmers

--- ProGrammers/Lv3/level_3_Connecting_islands.py
+++ ProGrammers/Lv3/level_3_Connecting_islands.py
@@ -1,7 +1,7 @@
 def solution(n, costs):
     answer = 0
     costs.sort(key=lambda x:x[2]) # 비용기반 정렬
-    routes = set([costs[0][0]])
+    routes = {costs[0][0]} # set([costs[0][0]])
     while len(routes) != n:
         for i, cost in enumerate(costs):
             if cost[0] in routes and cost[1] in routes:
