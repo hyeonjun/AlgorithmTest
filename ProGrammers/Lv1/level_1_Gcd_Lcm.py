9bd1c2367805c053988202f8d2397c0a23007fc8
zlel175 <zlel175@naver.com>
2021-06-30 15:25:39 +0900

    Add Solution

--- ProGrammers/Lv1/level_1_Gcd_Lcm.py
+++ ProGrammers/Lv1/level_1_Gcd_Lcm.py
@@ -13,3 +13,13 @@ def solution(n, m):
 
 print(solution(3, 12))
 print(solution(2, 5))
+
+def solution(n, m):
+    def gcd(n, m):
+        return n if m==0 else gcd(m, n%m)
+    gn = gcd(n, m) if n > m else gcd(m, n)
+    lcd = n*m // gn
+    return [gn, lcd]
+
+print(solution(3, 12))
+print(solution(2, 5))
