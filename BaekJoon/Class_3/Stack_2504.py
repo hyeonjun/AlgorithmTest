8cedc3fcd43507a7e96fa4ff3e8fd5418d228b0a
zlel175 <zlel175@naver.com>
2021-10-04 09:12:28 +0900

    Add_Data

--- BaekJoon/Class_3/Stack_2504.py
+++ BaekJoon/Class_3/Stack_2504.py
@@ -11,29 +11,37 @@ stack = []
 
 for i in input:
     if i == ")":
-        tmp = 0
-        while stack:
-            x = stack.pop()
-            if x == '(':
-                stack.append(2 if tmp == 0 else tmp * 2)
-                break
-            elif x == '[':
-                print(0)
-                exit(0)
-            else:
-                tmp += x
+        if len(stack) > 0:
+            tmp = 0
+            while stack:
+                x = stack.pop()
+                if x == '(':
+                    stack.append(2 if tmp == 0 else tmp * 2)
+                    break
+                elif x == '[':
+                    print(0)
+                    exit(0)
+                else:
+                    tmp += x
+        else:
+            print(0)
+            exit(0)
     elif i == "]":
-        tmp = 0
-        while stack:
-            x = stack.pop()
-            if x == '[':
-                stack.append(3 if tmp == 0 else tmp * 3)
-                break
-            elif x == '(':
-                print(0)
-                exit(0)
-            else:
-                tmp += x
+        if len(stack) > 0:
+            tmp = 0
+            while stack:
+                x = stack.pop()
+                if x == '[':
+                    stack.append(3 if tmp == 0 else tmp * 3)
+                    break
+                elif x == '(':
+                    print(0)
+                    exit(0)
+                else:
+                    tmp += x
+        else:
+            print(0)
+            exit(0)
     else:
         stack.append(i)
 print(0 if '(' in stack or '[' in stack else sum(stack))
