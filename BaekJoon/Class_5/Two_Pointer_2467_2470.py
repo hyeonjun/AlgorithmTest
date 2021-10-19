b6fdb0165bac11dee614fcd86533c402ef594224
zlel175 <zlel175@naver.com>
2021-10-19 16:17:08 +0900

    Update

--- BaekJoon/Class_5/Two_Pointer_2467_2470.py
+++ BaekJoon/Class_5/Two_Pointer_2467_2470.py
@@ -1,5 +1,6 @@
+# 2467번 문제는 용액이 이미 오름차순으로 정렬되어 들어오지만 2470번 문제는 정렬된 상태가 아니다.
 n = int(input())
-solution = list(map(int, input().split()))
+solution = sorted(list(map(int, input().split())))
 left, right = 0, n-1
 answer = [abs(solution[left] + solution[right]), (solution[left], solution[right])]
 while left < right:
@@ -12,6 +13,6 @@ while left < right:
     elif tmp > 0:
         right -= 1
     else:
-        print(answer[1][0], answer[1][1])
+        print(*answer[1])
         exit(0)
-print(answer[1][0], answer[1][1])
+print(*answer[1])
