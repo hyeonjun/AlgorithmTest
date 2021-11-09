caa134cccea11fbd37aa5772847a8de5c4fcb599
zlel175 <zlel175@naver.com>
2021-11-09 12:20:26 +0900

    2021_11_09_BaekJoon_DP_Practice

--- BaekJoon/DP/DP_2624.py
+++ BaekJoon/DP/DP_2624.py
@@ -10,5 +10,14 @@ for i in range(1, k+1): # 코인 종류
         dp[i][j] = dp[i-1][j] # 기존 값을 불러옴
     for c in range(1, coin[i-1][1]+1): # 각 코인 개수
         for idx in range(c*coin[i-1][0], t+1): # 앞으로 만들어야할 값
-            dp[i][idx] += dp[i-1][idx-c*coin[i-1][0]]
+            dp[i][idx] += dp[i-1][idx-c*coin[i-1][0]] # 추가해야할 코인의 경우의 수 포함
 print(dp[k][t])
+
+"""
+20
+3
+5 3
+10 2
+1 5
+=> 4
+"""
