05f5f1cf22e37e7bd4a16fabe5b483c5ccab9b4a
zlel175 <zlel175@naver.com>
2022-01-19 01:11:51 +0900

    2022_01_18_BaekJoon_Math_Practice

---
+++ BaekJoon/AlgorithmType/Math/Math_Basic_2.py
@@ -0,0 +1,56 @@
+# 11382
+a, b, c = map(int, input().split())
+print(a+b+c)
+
+# 13277
+a, b = map(int, input().split())
+print(a*b)
+
+# 14652
+n, m, k = map(int, input().split())
+print(k//m, k%m)
+
+# 14928
+print(int(input()) % 20000303)
+
+# 15727
+import math
+print(math.ceil(int(input()) / 5))
+
+# 15740
+a, b = map(int, input().split())
+print(a+b)
+
+# 15894
+print(int(input()) * 4)
+
+# 15964
+a, b = map(int, input().split())
+print((a+b)*(a-b))
+
+# 16394
+print(int(input())-1946)
+
+# 16430
+a, b = map(int, input().split())
+print(b-a, b)
+
+# 17256
+ax, ay, az = map(int, input().split())
+cx, cy, cz = map(int, input().split())
+bx = cx - az
+by = cy // ay
+bz = cz - ax
+print(bx, by, bz)
+
+# 17496
+import math
+n, t, c, p = map(int, input().split())
+print((math.ceil(n/t)-1)*c*p)
+
+# 18108
+print(int(input())-543)
+
+# 20492
+n = int(input())
+print(int(n * 0.78), int(n * 0.8 + n * 0.2 * 0.78))
